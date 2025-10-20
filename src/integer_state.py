61dabb56c77ebd1fde13c32b61a7d487a94d7c45
onouviase <ono@alexi.com>
2025-10-20 09:46:05 -0400

    refactored IntegerStateMachine to support state transitions and input processing

--- src/integer_state.py
+++ src/integer_state.py
@@ -5,9 +5,25 @@
     File Name: integer_state.py
 """
 class IntegerStateMachine:
-    def __init__(self, initial_value: str):
-        self.value = int(initial_value, 2)
+    def __init__(self, states: set, input_values: set, initial_state: str, final_states: set, transitions: dict = None):
+        self.states = states
+        self.input_values = input_values
+        self.initial_state = initial_state
+        self.final_states = final_states
+        self.transitions = transitions if transitions else {}
+        self.value = int(initial_state)
         self.modulus_value = None
+
+    def process_input(self, values: str):
+        state = self.initial_state
+        for input_value in values:
+            if input_value not in self.input_values:
+                raise ValueError(f"Invalid input value: {input_value}. Allowed values are: {self.input_values}")
+            key = (state, input_value)
+            if key not in self.transitions:
+                raise ValueError(f"No transition defined for state {state} with input {input_value}")
+            state = self.transitions[key]
+        return state
 
 
 
