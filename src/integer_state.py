f66435589868608e589bf9a17d77aed350c51450
onouviase <ono@alexi.com>
2025-10-21 06:57:05 -0400

    changed IntegerStateMachine to use input_symbols and updated mod_three function for processing

--- src/integer_state.py
+++ src/integer_state.py
@@ -5,13 +5,12 @@
     File Name: integer_state.py
 """
 class IntegerStateMachine:
-    def __init__(self, states: set, input_values: set, initial_state: str, final_states: set, transitions: dict = None):
+    def __init__(self, states: set, input_symbols: set, initial_state: str, final_states: set, transitions: dict = None):
         self.states = states
-        self.input_values = input_values
+        self.input_values = input_symbols
         self.initial_state = initial_state
         self.final_states = final_states
         self.transitions = transitions if transitions else {}
-        self.value = int(initial_state)
         self.modulus_value = None
 
     def process_input(self, values: str):
@@ -30,12 +29,27 @@ class IntegerStateMachine:
 
 
 
-def mod_three(state: IntegerStateMachine):
-    state.modulus_value = state.value % 3
-    return state.modulus_value
+def mod_three(state_machine: IntegerStateMachine, binary_str: str):
+    state = state_machine.process_input(binary_str)
+    final_state_mappings = {'S0': 0, 'S1': 1, 'S2': 2}
+    return final_state_mappings.get(state)
 
 if __name__ == '__main__':
-    state = IntegerStateMachine('1111')
-    result = mod_three(state)
-    print(f"Modulus of {state.value} by 3 is: {result}")
+    states = {'S0', 'S1', 'S2'}
+    input_symbols = {'0', '1'}
+    initial_state = 'S0'
+    final_states = {'S0', 'S1', 'S2'}
+    transitions = {
+        ('S0', '0'): 'S0',
+        ('S0', '1'): 'S1',
+        ('S1', '0'): 'S2',
+        ('S1', '1'): 'S0',
+        ('S2', '0'): 'S1',
+        ('S2', '1'): 'S2',
+    }
 
+    fsm = IntegerStateMachine(states, input_symbols, initial_state, final_states, transitions)
+
+    binary_input = "1101"
+    result = mod_three(fsm, binary_input)
+    print(f"The integer value of binary '{binary_input}' modulo 3 is: {result}")
