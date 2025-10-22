635fa600fe720abdab19682da221f90309b3ea02
onouviase <ono@alexi.com>
2025-10-22 14:05:22 -0400

    changed from `mod_three` function to ModThreeStateMachine class for modulo 3 state processing (OOP)

--- src/modulo_functions.py
+++ src/modulo_functions.py
@@ -6,7 +6,25 @@
 """
 from src.integer_state import IntegerStateMachine
 
-def mod_three(state_machine: IntegerStateMachine, binary_str: str) -> int:
-    state = state_machine.process_input(binary_str)
-    final_state_mappings = {'S0': 0, 'S1': 1, 'S2': 2}
-    return final_state_mappings.get(state)
+class ModThreeStateMachine(IntegerStateMachine):
+    def __init__(self):
+        states = {'S0', 'S1', 'S2'}
+        input_values = {'0', '1'}
+        initial_state = 'S0'
+        final_states = {'S0', 'S1', 'S2'}
+        transitions = {
+            ('S0', '0'): 'S0',
+            ('S0', '1'): 'S1',
+            ('S1', '0'): 'S2',
+            ('S1', '1'): 'S0',
+            ('S2', '0'): 'S1',
+            ('S2', '1'): 'S2',
+        }
+
+        super().__init__(states=states, input_values=input_values, initial_state=initial_state,
+                         final_states=final_states, transitions=transitions)
+
+    def mod_three(self, binary_str: str) -> int:
+        state = self.process_input(binary_str)
+        final_state_mappings = {'S0': 0, 'S1': 1, 'S2': 2}
+        return final_state_mappings.get(state)
